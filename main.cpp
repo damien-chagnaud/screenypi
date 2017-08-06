0484853a03c2dffbeb20abcde4e66b929e7db644
damien <damien-chagnaud@wanadoo.fr>
2017-08-06 17:06:04 -0400

    Et on recomense le tout

--- main.cpp
+++ main.cpp
@@ -3,16 +3,29 @@
 #include <QtQml/QQmlContext>
 #include <QtWebView/QtWebView>
 
+
+
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+
     QGuiApplication app(argc, argv);
     QtWebView::initialize();
 
     QQmlApplicationEngine engine;
     QQmlContext *context = engine.rootContext();
 
+//A) Load Conf files
 
-   // engine.rootContext()->setContextProperty(QStringLiteral("initialUrl"), initialUrl);
+
+//B) Load and configure RESTFUl system
+
+
+//C) configure player
+
+
+//D) Load player
 
     engine.load(QUrl(QStringLiteral("qrc:/admin.qml")));
 
